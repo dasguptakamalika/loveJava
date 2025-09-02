dde1704ae8b0f01a58f71dfb4fccba065d391434
Kams <159199110+dasguptakamalika@users.noreply.github.com>
2025-09-02 17:42:53 -0400

    Add files via upload
    class Dog
    String name;

---
+++ Main.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args)
+    {
+        Dog d = new Dog();
+        d.name = "Whiskey";
+        System.out.println(d.name + " : " + "The goodest Girl");
+    }
+}
+//new Dog() = object in memory.
+
+//d = reference pointing to that object.
