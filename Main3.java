d7ffeba79bb8ea4959908f503ac13b9db4031842
Kams <159199110+dasguptakamalika@users.noreply.github.com>
2025-09-27 23:41:41 -0400

    Update Main3.java

--- Main3.java
+++ Main3.java
@@ -4,7 +4,7 @@ public class Main3 {
     public static void main(String[] args) {
         String s1 = "Hello";
         String s2 = "Hello";
-        String s3 = new String("Hello");
+        String s3 = new String("Hello"); 
         String s4 = new String("Hello");
 
         System.out.println(s1==s2);
@@ -22,4 +22,5 @@ public class Main3 {
      ↑       ↑
         s1      s2
     }
+
 }
