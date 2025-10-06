fa6b59e17d9da4985ad0207e767e74d605c20c5a
Kams <159199110+dasguptakamalika@users.noreply.github.com>
2025-10-06 10:14:24 -0400

    Update Main3.java

--- Main3.java
+++ Main3.java
@@ -5,7 +5,7 @@ public class Main3 {
         String s1 = "Hello";
         String s2 = "Hello";
         String s3 = new String("Hello"); 
-        String s4 = new String("Hello");
+        String s4 = new String("Hello"); 
 
         System.out.println(s1==s2);
         System.out.println(s3.equals(s4));
@@ -24,3 +24,4 @@ public class Main3 {
     }
 
 }
+
