f8ba256697131f42d7c778d6f45d90963691961a
Kams <159199110+dasguptakamalika@users.noreply.github.com>
2025-11-28 23:12:13 -0500

    Update Main2.java

--- Main2.java
+++ Main2.java
@@ -9,10 +9,11 @@ public class Main2 {
         d2.name = "Whiskers";
         System.out.println(d2.name + " " + d1.name);
         System.out.println(d2.name==d1.name);
-        System.out.println(d2.name.equals(d1.name));
+        System.out.println(d2.name.equals(d1.name)); 
     }
 }
 /*Dog d2 = d1; makes both variables point to the same object.
 So changing d2.name changes the same Dog that d1 references →
 
 both print “Whiskers”.*/ 
+
